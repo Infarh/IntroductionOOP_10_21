8f030cc47d68bf11135ce57c89fbb757480f168a
Infarh <shmachilin@yandex.ru>
2021-10-22 20:36:05 +0300

    Добавьте файлы проекта.

---
+++ IntroductionOOP/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IntroductionOOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
