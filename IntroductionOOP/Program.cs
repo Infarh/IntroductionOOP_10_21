8ddd554761199ab4fbec297e1b18dcf2a6915bb7
Infarh <shmachilin@yandex.ru>
2021-10-29 21:19:06 +0300

    Логика работы двусвязного списка

--- IntroductionOOP/Program.cs
+++ IntroductionOOP/Program.cs
@@ -1,11 +1,24 @@
-﻿namespace IntroductionOOP
+﻿using System.Collections.Generic;
+using System.Linq;
+using Utilities;
+
+namespace IntroductionOOP
 {
     class Program
     {
         static void Main(string[] args)
         {
+            LinkedList<int> list1 = new();
+            var node = list1.AddFirst(42);
+            node.Value = 7;
 
+
+            RefList<int> list = new(Enumerable.Range(1, 10));
+
+            var target = list.First.Next.Next;
+
+            var after_node = list.AddAfter(target, 0);
+            var before_node = list.AddBefore(target, -1);
         }
     }
-
 }
---
+++ Utilities/RefList.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace Utilities
+{
+    [DebuggerDisplay("RefList[items:{Count}]")]
+    public class RefList<T>
+    {
+        [DebuggerDisplay("Node = {Value}")]
+        public class Node
+        {
+            internal RefList<T> _List;
+
+            public T Value { get; set; }
+
+            public Node Next { get; internal set; }
+
+            public Node Prev { get; internal set; }
+
+            internal Node(RefList<T> List,T Value)
+            {
+                _List = List;
+                this.Value = Value;
+            }
+
+            //public override string ToString() => Value?.ToString();
+        }
+
+        private int _Count;
+
+        public Node First { get; private set; }
+
+        public Node Last { get; private set; }
+
+        public int Count => _Count;
+
+        public RefList() { }
+
+        public RefList(IEnumerable<T> items)
+        {
+            foreach (var item in items)
+                AddLast(item);
+        }
+
+        public Node AddFirst(T value)
+        {
+            var node = new Node(this, value);
+
+            _Count++;
+
+            if (First is null)
+            {
+                First = node;
+                Last = node;
+                return node;
+            }
+
+            node.Next = First;
+            First.Prev = node;
+            First = node;
+
+            return node;
+        }
+
+        public Node AddLast(T value)
+        {
+            var node = new Node(this, value);
+
+            _Count++;
+
+            if (Last is null)
+            {
+                First = node;
+                Last = node;
+                return node;
+            }
+
+            node.Prev = Last;
+            Last.Next = node;
+            Last = node;
+
+            return node;
+        }
+
+        public Node AddAfter(Node Position, T value)
+        {
+            if (!ReferenceEquals(Position._List, this))
+                throw new InvalidOperationException("Произведена попытка добавления элемента в список после узла, принадлежащего другому списку");
+
+            if (ReferenceEquals(Position, Last))
+                return AddLast(value);
+
+            var node = new Node(this, value)
+            {
+                Prev = Position,
+                Next = Position.Next,
+            };
+
+            Position.Next = node;
+            node.Next.Prev = node;
+
+            return node;
+        }
+
+        public Node AddBefore(Node Position, T value)
+        {
+            if (!ReferenceEquals(Position._List, this))
+                throw new InvalidOperationException("Произведена попытка добавления элемента в список до узла, принадлежащего другому списку");
+
+            if (ReferenceEquals(Position, Last))
+                return AddLast(value);
+
+            var node = new Node(this, value)
+            {
+                Next = Position,
+                Prev = Position.Prev,
+            };
+
+            Position.Prev = node;
+            node.Prev.Next = node;
+
+            return node;
+        }
+
+        public T Remove(Node node)
+        {
+            if (!ReferenceEquals(node._List, this))
+                throw new InvalidOperationException("Произведена попытка удаления узла, принадлежащего другому списку");
+
+            node._List = null;
+
+            if (_Count == 1)
+            {
+                First = null;
+                Last = null;
+            }
+            else if (ReferenceEquals(First, node))
+            {
+                First = node.Next;
+                First.Prev = null;
+            }
+            else if (ReferenceEquals(Last, node))
+            {
+                Last = node.Prev;
+                Last.Next = null;
+            }
+            else
+            {
+                node.Prev.Next = node.Next;
+                node.Next.Prev = node.Prev;
+            }
+
+            node.Next = null;
+            node.Prev = null;
+
+            _Count--;
+            return node.Value;
+        }
+
+        public void Clear()
+        {
+            if (_Count == 0) return;
+            var node = First;
+            while (node != null)
+            {
+                node._List = null;
+                node.Prev = null;
+                var tmp = node;
+                node = node.Next;
+                tmp.Next = null;
+            }
+        }
+    }
+}
