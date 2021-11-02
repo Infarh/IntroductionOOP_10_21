b09c01e5a990d58184e7d4e4d374e41d2a2c9a22
Infarh <shmachilin@yandex.ru>
2021-11-02 22:13:02 +0300

    Примеры переопределения конструкторов, операторов, методов

--- IntroductionOOP/Program.cs
+++ IntroductionOOP/Program.cs
@@ -1,14 +1,27 @@
 ﻿//#define IncludeInternal
 
 using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Reflection.Metadata;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Text.Json;
+using System.Xml.Serialization;
+using Utilities;
+using Utilities.Extensions;
 
 namespace IntroductionOOP
 {
-    public record Person(string LastName, string FirstName, string Patronymic);
+    [Serializable]
+    public record Person(string LastName, string FirstName, string Patronymic)
+    {
+        public Person() : this("", "", "")
+        {
+            
+        }
+    }
 
     class Program
     {
@@ -20,6 +33,29 @@ namespace IntroductionOOP
 
             var rnd = new Random(5);
             var persons = CreatePersons(1000, first, last, patron, rnd).ToArray();
+
+            var xml_serializer = new XmlDataSerializer<Person>();
+            var json_serializer = new JsonDataSerializer<Person>();
+            var bin_serializer = new BinDataSerializer<Person>();
+
+            Serialize("FirstPerson.xml", persons[0], xml_serializer);
+            Serialize("SecondPerson.json", persons[1], json_serializer);
+            Serialize("Person3.bin", persons[3], bin_serializer);
+
+            var vector = new Vector3D(1, 2, 3);
+
+            var v2 = vector * 5;
+            var v3 = 6 * vector;
+
+            var len = (double)vector;
+
+            var (x, y, z) = vector;
+        }
+
+        private static void Serialize(string FileName, Person person, DataSerializer<Person> serializer)
+        {
+            using var file = File.Create(FileName);
+            serializer.Serialize(person, file);
         }
 
         private static void ReadData(
@@ -38,9 +74,14 @@ namespace IntroductionOOP
                 if (components.Length < 3)
                     continue;
 
-                last.Add(components[0]);
-                first.Add(components[1]);
-                patron.Add(components[2]);
+                //var (f, l, p) = components;
+                string last_name;
+                (var first_name, last_name, var patronymic_name) = components;
+                //components.Deconstruct(out var first_name, out last_name, out var patronymic_name);
+
+                last.Add(last_name);
+                first.Add(first_name);
+                patron.Add(patronymic_name);
             }
 
             //last.Capacity = last.Count;
@@ -90,6 +131,110 @@ namespace IntroductionOOP
 
                 yield return new Person(last_name, first_name, patronymic);
             }
+        }
+    }
+
+    public abstract class DataSerializer<T>
+    {
+        public abstract void Serialize(T value, Stream stream);
+
+        public abstract T Deserialize(Stream stream);
+    }
+
+    public static class XmlSerializersPool
+    {
+        private static readonly ConcurrentDictionary<Type, XmlSerializer> __Serializers = new();
+
+        public static XmlSerializer GetSerializer(Type type) => __Serializers.GetOrAdd(type, t => new XmlSerializer(t));
+
+        public static XmlSerializer GetSerializer<T>() => GetSerializer(typeof(T));
+
+        public static XmlSerializer GetSerializer(object value) => GetSerializer(value.GetType());
+
+        public static void Serialize(object value, Stream stream) => GetSerializer(value).Serialize(stream, value);
+
+        public static T Deserialize<T>(Stream stream) => (T)GetSerializer<T>().Deserialize(stream);
+    }
+
+    public class XmlDataSerializer<T> : DataSerializer<T>
+    {
+        //private XmlSerializer _Serializer = XmlSerializersPool.GetSerializer<T>();
+
+        //public XmlDataSerializer()
+        //{
+        //    _Serializer = XmlSerializersPool.GetSerializer(typeof(T));
+        //}
+
+        public override void Serialize(T value, Stream stream) => XmlSerializersPool.Serialize(value, stream);
+
+        public override T Deserialize(Stream stream) => XmlSerializersPool.Deserialize<T>(stream);
+    }
+
+    public class JsonDataSerializer<T> : DataSerializer<T>
+    {
+        private readonly Encoding _Encoding;
+
+        //public JsonDataSerializer() => _Encoding = Encoding.UTF8;
+        public JsonDataSerializer() : this(Encoding.UTF8) { }
+
+        public JsonDataSerializer(Encoding encoding) => _Encoding = encoding;
+
+        public override void Serialize(T value, Stream stream)
+        {
+            var str = JsonSerializer.Serialize(value);
+            //var encoding = Encoding.UTF8;
+            var bytes = _Encoding.GetBytes(str);
+            stream.Write(bytes);
+        }
+
+        public override T Deserialize(Stream stream)
+        {
+            //using var other_reader = new BinaryReader(stream, _Encoding, true);
+            var reader = new StreamReader(stream, _Encoding);
+            var str = reader.ReadToEnd();
+            var result = (T)JsonSerializer.Deserialize(str, typeof(T));
+            return result;
+
+            //using (var reader = new StreamReader(stream))
+            //{
+            //    //...
+            //}
+
+            //StreamReader reader = null;
+            //try
+            //{
+            //    reader = new StreamReader(stream);
+
+            //    // ...
+            //}
+            //finally
+            //{
+            //    reader?.Dispose();
+            //}
+
+        }
+    }
+
+    public class BinDataSerializer<T> : DataSerializer<T>
+    {
+        private BinaryFormatter _Formatter = new();
+
+        public override void Serialize(T value, Stream stream)
+        {
+#pragma warning disable SYSLIB0011 // Тип или член устарел
+#pragma warning disable 618
+            _Formatter.Serialize(stream, value);
+#pragma warning restore 618
+#pragma warning restore SYSLIB0011 // Тип или член устарел
+        }
+
+        public override T Deserialize(Stream stream)
+        {
+#pragma warning disable SYSLIB0011 // Тип или член устарел
+#pragma warning disable 618
+            return (T)_Formatter.Deserialize(stream);
+#pragma warning restore 618
+#pragma warning restore SYSLIB0011 // Тип или член устарел
         }
     }
 }
---
+++ Utilities/Vector3D.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utilities
+{
+    public readonly partial struct Vector3D
+    {
+        private readonly double _X;
+        private readonly double _Y;
+        private readonly double _Z;
+
+        public double X => _X;
+        public double Y => _Y;
+        public double Z => _Z;
+
+        public double Length => Math.Sqrt(_X * _X + _Y * _Y + _Z * _Z);
+
+        private static (double x, double y, double z) GetRandomVector(double Length)
+        {
+            var rnd = new Random();
+            var x = rnd.NextDouble();
+            var y = rnd.NextDouble();
+            var z = rnd.NextDouble();
+
+            var len = Math.Sqrt(x * x + y * y + z * z);
+
+            var k = Length / len;
+
+            return (x * k, y * k, z * k);
+        }
+
+        public Vector3D(double Length) : this(GetRandomVector(Length))
+        {
+
+        }
+
+        private Vector3D((double x, double y, double z) value) : this(value.x, value.y, value.z) { }
+
+        public Vector3D(double X, double Y, double Z)
+        {
+            //(_X, _Y, _Z) = (X, Y, Z);
+            _X = X;
+            _Y = Y;
+            _Z = Z;
+        }
+
+       
+
+        public void Deconstruct(out double X, out double Y, out double Z) => (X, Y, Z) = (_X, _Y, _Z);
+
+    }
+}
